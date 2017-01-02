21a640272830c0b78dfbea896a8afb312ba2e927
Milan Rafaeli <mbrafaeli@uc.cl>
2017-01-02 19:06:50 -0300

    2º commit

---
+++ Code/Groups.py
@@ -0,0 +1,39 @@
+
+
+class Groups:
+    def __init__(self):
+        self.participantes = dict
+        self.Admin = None
+        self.picture = None
+        self.Description = None
+        self.Recorrido = None
+        self.chat = None
+        self.historial = None
+        self.commit_trips = None
+        self.queue_trips = None
+
+    def add_participant(self):
+        pass
+
+    def kick_participant(self):
+        pass
+
+    def create_free_ride_trip(self):
+        pass
+
+    def remove_free_ride_trips(self):
+        pass
+
+    def edit_Description(self, new_description):
+        self.Description = new_description
+
+    def create_commit_trip(self):
+        pass
+
+    def edit_commit_trip(self):
+        pass
+
+    def find_trip(self):
+        pass
+
+
